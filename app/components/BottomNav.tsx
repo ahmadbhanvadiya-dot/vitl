6f7bb97221fac5c944fa3c3e416fa8601d4616d1
ahmadbhanvadiya-dot <ahmadbhanvadiya@gmail.com>
2026-06-03 23:54:33 +0530

    BottomNav

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,135 +1,50 @@
 "use client";
 
 import Link from "next/link";
-import { Home, Clock3, QrCode, User } from "lucide-react";
+import { Home, Pill, QrCode, User } from "lucide-react";
 
 export default function BottomNav() {
-
   return (
-
-    <div
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        background: "white",
-        borderTop: "1px solid #e5e7eb",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        padding: "12px 0",
-        zIndex: 999,
-      }}
-    >
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 w-[95%] max-w-md bg-white border border-gray-200 shadow-lg rounded-3xl px-6 py-3 flex justify-between items-center z-50">
 
       <Link
         href="/dashboard"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textDecoration: "none",
-        }}
+        className="flex flex-col items-center text-red-700"
       >
-
-        <Home
-          size={24}
-          color="#b91c1c"
-        />
-
-        <span
-          style={{
-            fontSize: "12px",
-            marginTop: "4px",
-            color: "#b91c1c",
-            fontWeight: 600,
-          }}
-        >
+        <Home size={22} />
+        <span className="text-xs mt-1 font-medium">
           Home
         </span>
-
       </Link>
 
       <Link
-        href="#"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textDecoration: "none",
-        }}
+        href="/medicines"
+        className="flex flex-col items-center text-gray-500"
       >
-
-        <Clock3
-          size={24}
-          color="#4b5563"
-        />
-
-        <span
-          style={{
-            fontSize: "12px",
-            marginTop: "4px",
-            color: "#4b5563",
-          }}
-        >
-          History
+        <Pill size={22} />
+        <span className="text-xs mt-1">
+          Medicines
         </span>
-
       </Link>
 
       <Link
-        href="#"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textDecoration: "none",
-        }}
+        href="/qr"
+        className="flex flex-col items-center text-gray-500"
       >
-
-        <QrCode
-          size={24}
-          color="#4b5563"
-        />
-
-        <span
-          style={{
-            fontSize: "12px",
-            marginTop: "4px",
-            color: "#4b5563",
-          }}
-        >
+        <QrCode size={22} />
+        <span className="text-xs mt-1">
           QR
         </span>
-
       </Link>
 
       <Link
         href="/profile"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textDecoration: "none",
-        }}
+        className="flex flex-col items-center text-gray-500"
       >
-
-        <User
-          size={24}
-          color="#4b5563"
-        />
-
-        <span
-          style={{
-            fontSize: "12px",
-            marginTop: "4px",
-            color: "#4b5563",
-          }}
-        >
+        <User size={22} />
+        <span className="text-xs mt-1">
           Profile
         </span>
-
       </Link>
 
     </div>
